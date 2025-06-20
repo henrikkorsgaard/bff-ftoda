723f5ed06f652ece34404a39951032dd83141e68
Henrik Korsgaard <henrik.korsgaard@knowit.dk>
2025-06-20 23:34:35 +0200

    feat: added GetLovForslag service endpoint

--- internal/ftoda/service.go
+++ internal/ftoda/service.go
@@ -20,7 +20,7 @@ func NewFTODAService() FTODAService {
 	}
 }
 
-func (s *FTODAService) GetLovforslagById(id int) {
+func (s *FTODAService) GetLovforslagById(id int) (sag Sag, err error) {
 	//First we should check a database, but that is not created yet
 	//If not found in database, then we get it from the api
 
@@ -29,16 +29,44 @@ func (s *FTODAService) GetLovforslagById(id int) {
 		filter: "typeid eq 3 and id eq " + strconv.Itoa(id),
 	}
 
+	// this need to be moved into a different repo service
 	odata, err := s.api.getData(query)
 	if err != nil {
 		fmt.Printf("error from getLovforslag: %s\n", err)
+		return sag, err
+	}
+
+	// this need to be moved into a different repo service
+	var sager []Sag
+	err = json.Unmarshal(odata.Result, &sager)
+	if err != nil {
+		fmt.Printf("error from getLovforslag: %s\n", err)
+		return sag, err
+	}
+
+	return sager[0], nil
+}
+
+// offset map into skip next for now
+func (s *FTODAService) GetLovforslag(offset int) ([]Sag, error) {
+	query := odataQuery{
+		entity: "Sag",
+		filter: "typeid eq 3",
+		skip:   offset,
+	}
+
+	odata, err := s.api.getData(query)
+	if err != nil {
+		fmt.Printf("error from getLovforslag: %s\n", err)
+		return nil, err
 	}
 
 	var sager []Sag
 	err = json.Unmarshal(odata.Result, &sager)
 	if err != nil {
 		fmt.Printf("error from getLovforslag: %s\n", err)
+		return nil, err
 	}
 
-	//fmt.Printf("%+v\n", sager)
+	return sager, nil
 }
